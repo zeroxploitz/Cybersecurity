8c3a62c2f9bd8f3327e426b132cc1ad4fcf4849a
Melvin Felix <53129990+melboogz@users.noreply.github.com>
2019-10-09 14:58:29 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,13 @@
 #!/usrs/bin/env python
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:77"
+from pip._vendor.distlib.compat import raw_input
+
+interface = raw_input("interface >")
+new_mac = raw_input("new MAC >")
 
 print("[+] Changing Mac address for " + interface + " to " + new_mac)
 
-subprocess.call("sudo ifconfig " + interface + " down ", shell=True)
-subprocess.call("sudo ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("sudo ifconfig " + interface + " up ", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
