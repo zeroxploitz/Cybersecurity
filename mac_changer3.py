096d7c721703fc3081ceeb99274f021581de197b
Melvin Felix <53129990+melboogz@users.noreply.github.com>
2019-10-10 09:50:51 -0400

    Add files via upload
    Mac Changer coded for python 3

---
+++ mac_changer3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import subprocess
+
+
+
+interface = input("interface >")
+new_mac = input("new MAC >")
+
+print("[+] Changing Mac address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
