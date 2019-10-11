0742c236b4f14248a928d6615315e7b95f1116c9
Melvin Felix <53129990+melboogz@users.noreply.github.com>
2019-10-11 10:01:46 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -8,8 +8,12 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its Mac address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
-    return parser.parse_args()
-
+    (options, arguments) = parser.parse_args()
+    if options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    return options
 
 def change_mac (interface, new_mac):
     print("[+] Changing Mac address for " + interface + " to " + new_mac)
@@ -18,5 +22,6 @@ def change_mac (interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
+
