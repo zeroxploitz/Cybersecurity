58454b7d0e4c87466542475987cda4b00f637de9
Melvin Felix <53129990+melboogz@users.noreply.github.com>
2019-10-14 22:19:45 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,7 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its Mac address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
     (options, arguments) = parser.parse_args()
-    if options.interface:
+    if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info")
     elif not options.new_mac:
         parser.error("[-] Please specify a new mac, use --help for more info")
