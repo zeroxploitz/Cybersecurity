dd4c913b561f73180657d63402dc6388740c62f9
Melvin Felix <53129990+melboogz@users.noreply.github.com>
2019-11-07 02:49:12 -0500

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python
+!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP/ IP range.")
+    options = parser.parse_args()
+    return options
 
 
 def scan(ip):
@@ -9,9 +17,17 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_results(results_list):
     print("IP\t\t\tMac Address\n-------------------------------------")
-    for element  in answered_list:
-        print(element[1].psrc+ "\t\t" + element[1].hwsrc)
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
-
-scan("192.168.2.1/24")
+options = get_arguments()
+scan_result = scan("options.target")
+print_results(scan_result)
